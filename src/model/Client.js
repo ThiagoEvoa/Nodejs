8c3717e6f45ee74c342096bb5e130c8251ed052c
Thiago <thiagoevoa@gmail.com>
2018-09-30 08:08:38 -0300

    Adding github

--- src/model/Client.js
+++ src/model/Client.js
@@ -3,18 +3,18 @@ const schema = mongoose.Schema
 const cpfValidator = require('../validator/CpfValidator')
 
 const clientSchema = new schema({
-    cpf:{
-        type:String,
-        required:true,
-        validate:[(cpf)=>{
+    cpf: {
+        type: String,
+        required: true,
+        validate: [(cpf) => {
             return cpfValidator.validateCpf(cpf)
         }]
     },
-    name:{
-        type:String,
-        required:true,
-        maxlength:[50],
-        minlength:[10]
+    name: {
+        type: String,
+        required: true,
+        maxlength: [50],
+        minlength: [10]
     }
 })
 
